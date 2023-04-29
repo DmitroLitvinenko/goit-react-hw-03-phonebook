1b5372433c223bbda030b2a85cdd19fd76fa48a5
Dmytro Litvinenko <dima.litvinenko1990@gmail.com>
2023-04-29 15:56:01 +0300

    complete save to localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,7 @@ import { Filter } from './Filter/Filter';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Container } from './ContactForm/ContactForm.styled';
-
-const InitialContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+import initialContacts from '../initialContacts.json';
 
 export class App extends Component {
   state = {
@@ -24,11 +18,11 @@ export class App extends Component {
     if (savedContacts !== null) {
       this.setState({ contacts: JSON.parse(savedContacts) });
     } else {
-      this.setState({ contacts: InitialContacts });
+      this.setState({ contacts: initialContacts });
     }
   }
 
-  componentDidUpdate(prevprops, prevState) {
+  componentDidUpdate(prevState) {
     if (prevState.contacts !== this.state.contacts) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
